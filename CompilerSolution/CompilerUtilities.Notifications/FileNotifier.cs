e5bf547345453fdc8fc791b7ada4803b8093413c
Kwelifin <Kwelifinwork@gmail.com>
2018-02-28 01:30:13 +0300

    Add FileNotifier

---
+++ CompilerSolution/CompilerUtilities.Notifications/FileNotifier.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using CompilerUtilities.Notifications.Interfaces;
+using CompilerUtilities.Notifications.Structs.Enums;
+
+namespace CompilerUtilities.Notifications
+{
+    public class FileNotifier:INotifier
+    {
+        private StreamWriter _fileWriter;
+        private INotifier _decoratedNotifier;
+        private static object _notifySyncObject;
+
+        public FileNotifier(string path, FileMode fileMode)
+        {
+            _fileWriter = new StreamWriter(new FileStream(path, fileMode));
+        }
+
+        public FileNotifier(INotifier decoratedNotifier, string path, FileMode fileMode):this(path, fileMode)
+        {
+            _decoratedNotifier = decoratedNotifier;
+        }
+
+        public void Notify(NotifyLevel level, string message)
+        {
+            lock (_notifySyncObject)
+                _fileWriter.Write($"{level.ToString()}:{message}");
+            _decoratedNotifier?.Notify(level, message);
+        }
+    }
+}
