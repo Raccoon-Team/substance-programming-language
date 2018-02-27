33aa29daa788e18f7960fc8269b97cfc3d0d6e80
Kwelifin <Kwelifinwork@gmail.com>
2018-02-28 01:11:41 +0300

    Add console notifier

---
+++ CompilerSolution/CompilerUtilities.Notifications/ConsoleNotifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CompilerUtilities.Notifications.Interfaces;
+using CompilerUtilities.Notifications.Structs.Enums;
+
+namespace CompilerUtilities.Notifications
+{
+    public class ConsoleNotifier:INotifier
+    {
+        private INotifier _decoratedNotifier;
+
+        public ConsoleNotifier() {}
+
+        public ConsoleNotifier(INotifier decoratedNotifier)
+        {
+            _decoratedNotifier = decoratedNotifier;
+        }
+
+        public void Notify(NotifyLevel level, string message)
+        {
+            Console.Out.WriteLine($"{level.ToString()}:{0}");
+            _decoratedNotifier?.Notify(level, message);
+        }
+    }
+}
