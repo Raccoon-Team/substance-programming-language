bf42191c7e0f275ba03e53e13967d39c3487a25f
Kwelifin <Kwelifinwork@gmail.com>
2018-02-28 19:16:21 +0300

    Add default notifier class

---
+++ CompilerSolution/CompilerUtilities.Notifications/DefaultNotifier.cs
@@ -0,0 +1,20 @@
+﻿using CompilerUtilities.Notifications.Interfaces;
+using CompilerUtilities.Notifications.Structs.Enums;
+
+namespace CompilerUtilities.Notifications
+{
+    public static class DefaultNotifier
+    {
+        private static INotifier _defaultNotifier;
+
+        public static void InitializeNotifier(INotifier newNotifier)
+        {
+            _defaultNotifier = newNotifier;
+        }
+
+        public static void Notify(NotifyLevel level, string message)
+        {
+            _defaultNotifier.Notify(level, message);
+        }
+    }
+}
