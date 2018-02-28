9f197495ccf8e17d5f4c05c42d0d4f9ca77b1bce
Kwelifin <Kwelifinwork@gmail.com>
2018-03-01 00:52:57 +0300

    Add separation log files by date and time

--- CompilerSolution/CompilerUtilities.Notifications/DefaultNotifier.cs
+++ CompilerSolution/CompilerUtilities.Notifications/DefaultNotifier.cs
@@ -1,11 +1,25 @@
-﻿using CompilerUtilities.Notifications.Interfaces;
+﻿using System;
+using System.IO;
+using CompilerUtilities.Notifications.Interfaces;
 using CompilerUtilities.Notifications.Structs.Enums;
 
 namespace CompilerUtilities.Notifications
 {
     public static class DefaultNotifier
     {
+        public static string DefaultLogDirectory = "Logs";
+
         private static INotifier _defaultNotifier;
+
+        public static void InitializeFileLoggerNotifier(INotifier anotherNotifier)
+        {
+            var TodayLogDirectoryPath = Path.Combine(DefaultLogDirectory, DateTime.Now.ToShortDateString());
+            Directory.CreateDirectory(TodayLogDirectoryPath);
+
+            var TimeForLogFileName = DateTime.Now.ToShortTimeString().Replace(':', ';');
+            var LogFilePath = Path.Combine(TodayLogDirectoryPath, $"{TimeForLogFileName}.txt");
+            _defaultNotifier = new FileNotifier(anotherNotifier, LogFilePath);
+        }
 
         public static void InitializeNotifier(INotifier newNotifier)
         {
